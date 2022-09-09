adb2004fafc6f7f0efd2e8a6b23e223b810cbe24
kkanto <108447856+kkanto@users.noreply.github.com>
2022-09-09 23:46:25 +0200

    Create maven project

---
+++ src/main/java/de/allianz/testupload1112/Testupload1112Application.java
@@ -0,0 +1,13 @@
+package de.allianz.testupload1112;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testupload1112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testupload1112Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/testupload1112/Testupload1112ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.testupload1112;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testupload1112ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
